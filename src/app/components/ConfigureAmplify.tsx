065f521e13c5c8227624377c6040e2b2446cb3e2
baaat27 <ssping111@gmail.com>
2025-09-28 15:19:40 +0900

    20250928

--- src/app/components/ConfigureAmplify.tsx
+++ src/app/components/ConfigureAmplify.tsx
@@ -4,7 +4,7 @@
 import { Amplify } from "aws-amplify";
 import config from '@/amplify_outputs.json';
 
-// Amplifyライブラリにバックエンドの設定情報を読み込ませる.
+// Amplifyライブラリにバックエンドの設定情報を読み込ませる
 Amplify.configure(config, { ssr: true });
 
 export default function ConfigureAmplifyClientSide() {
